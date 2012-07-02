bd92b95d49dd5171409ca671684681845b3b34b2
Aard-Jan van Kesteren <aj@tictech.nl>
2012-07-02 17:10:02 +0200

    -

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,3 @@
 cd ..
 python /Applications/s3cmd-1.1.0-beta3/s3cmd sync --acl-public --rr -r . s3://www.tictech.nl
+python /Applications/s3cmd-1.1.0-beta3/s3cmd sync --acl-public --rr -r . s3://tictech.nl
